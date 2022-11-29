73b6bae1131e5adef93a4da3efbaa849969569f5
Ruiqi Zhu <zhuruiqi1@gmail.com>
2022-11-29 16:59:52 +0000

    add saute double pendulum env

--- randomizer/safe_env/double_pendulum/safe_double_pendulum.py
+++ randomizer/safe_env/double_pendulum/safe_double_pendulum.py
@@ -129,7 +129,7 @@ class SautedRandomizableDoublePendulumEnv(RandomizeSafeDoublePendulumEnv):
             min_rel_budget: float = 1.,  # minimum relative (with respect to safety_budget) budget
             max_rel_budget: float = 1.,  # maximum relative (with respect to safety_budget) budget
             test_rel_budget: float = 1.,  # test relative budget
-            unsafe_reward: float = 0,
+            unsafe_reward: float = -200.,
             use_reward_shaping: bool = True,  # ablation
             use_state_augmentation: bool = True,  # ablation
             **kwargs
